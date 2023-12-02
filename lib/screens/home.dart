b82db8c53d541945a17a664c8319e4f88e33921b
sirisandnes <siribsa@stud.ntnu.no>
2023-12-02 18:06:37 +0100

    changed timer

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,12 @@
 import 'dart:convert';
-import 'dart:ffi';
+
 import 'package:intl/intl.dart';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_project/models/exercise.dart';
 import 'package:final_project/models/user.dart' as myUser;
 import 'package:final_project/models/workout.dart';
 import 'package:final_project/widgets/home/chart/chart.dart';
 import 'package:final_project/widgets/home/horizontal_listview/horizontal_listview.dart';
-import 'package:final_project/widgets/home/timer/timer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -85,6 +83,21 @@ class _HomeScreenState extends State<HomeScreen> {
     return loadedWorkouts;
   }
 
+  int calculateWorkoutsForCurrentWeek(List<Workout> allWorkouts) {
+    print(allWorkouts.length);
+    DateTime now = DateTime.now();
+    DateTime weekStart = now.subtract(Duration(days: now.weekday - 1));
+    DateTime weekEnd = weekStart.add(Duration(days: 6));
+
+    List<Workout> workoutsInWeek = allWorkouts.where((workout) {
+      DateTime workoutDate = workout.date;
+      return workoutDate.isAfter(weekStart) && workoutDate.isBefore(weekEnd);
+    }).toList();
+
+    print(workoutsInWeek.length);
+    return workoutsInWeek.length;
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = const Center(
@@ -119,7 +132,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       children: [
                         Text(
                           "Hi! ${user!.firstname}",
-                          style: TextStyle(
+                          style: const TextStyle(
                             fontSize: 20,
                             color: Color.fromARGB(255, 26, 53, 121),
                           ),
@@ -174,9 +187,120 @@ class _HomeScreenState extends State<HomeScreen> {
                             workouts: workouts!,
                           );
                         }
-                        return HorizontalListView(workouts: []);
+                        return const HorizontalListView(workouts: []);
                       }),
-                  Timer(),
+                  FutureBuilder(
+                      future: _loadedWorkouts,
+                      builder: (context, snapshot2) {
+                        if (snapshot2.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        if (snapshot2.hasError) {
+                          return const Center(
+                            child: Text(
+                              "error",
+                            ),
+                          );
+                        }
+                        if (snapshot2.hasData) {
+                          final workouts = snapshot2.data;
+                          return Padding(
+                            padding: const EdgeInsets.fromLTRB(16, 24, 16, 8),
+                            child: Column(
+                              children: [
+                                const Row(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                                      child: Text(
+                                        "WORKOUTS THIS WEEK",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w800),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 8,
+                                ),
+                                Container(
+                                  height: 100,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    color:
+                                        const Color.fromARGB(255, 44, 88, 200),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      //HERE TIMER SHOULD BE FOR CURRENT WORKOUT
+                                      Text(
+                                        calculateWorkoutsForCurrentWeek(
+                                                workouts!)
+                                            .toString(),
+                                        textAlign: TextAlign.center,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 40,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          );
+                        }
+                        return Padding(
+                          padding: const EdgeInsets.fromLTRB(16, 24, 16, 8),
+                          child: Column(
+                            children: [
+                              const Row(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                                    child: Text(
+                                      "WORKOUTS THIS WEEK",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w800),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Container(
+                                height: 100,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  color: const Color.fromARGB(255, 44, 88, 200),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    //HERE TIMER SHOULD BE FOR CURRENT WORKOUT
+                                    Text(
+                                      calculateWorkoutsForCurrentWeek([])
+                                          .toString(),
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 40,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      })
                 ],
               ),
             ),
